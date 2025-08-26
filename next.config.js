a41f1ac731ea37db72eb17fcf2d5e3bc01957a2c
Hugo Wentzel <hugowentzel@gmail.com>
2025-08-26 13:52:27 -0400

    Fix CSP to allow Google Maps API loading - address autocomplete now working

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://maps.googleapis.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
